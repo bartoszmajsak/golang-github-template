489d6bd7ce27f9d6d89f3fa702b6d1574abea082
Bartosz Majsak <bartosz.majsak@gmail.com>
2021-12-11 13:03:19 +0100

    feat(init): reworks init script

--- init.sh
+++ init.sh
@@ -9,16 +9,15 @@ defaultName=$(git remote -v | cut -d':' -f 2 | cut -d'.' -f 1 | uniq)
 read -p "Repo name (${defaultName}): " name
 test -z "$name" && name=${defaultName}
 
-find . -type f -not -path "./vendor/*" -not -path "./.git/*" -not -path "./init.sh"  -exec sed -i "s|bartoszmajsak/template-golang|${name}|g" '{}' \;
-
-read -p "Project name: " project
-test -z "$name" && {
-  die "You must specify project name"
-}
-sed -i "s|PROJECT_NAME:=template-golang|PROJECT_NAME:=${project}|g" Makefile
+defaultProject=${defaultName#*/}
+read -p "Project name (${defaultProject}}: " project
+test -z "project" && project=${defaultProject}
 
 read -p "Executable name: " binary
-test -z "$name" && {
+test -z "binary" && {
   die "You must specify executable name"
 }
-sed -i "s|PROJECT_NAME:=template-golang|BINARY_NAME:=${binary}|g" Makefile
+
+find . -type f -not -path "./vendor/*" -not -path "./.git/*" -not -path "./init.sh"  -exec sed -i "s|bartoszmajsak/template-golang|${name}|g" '{}' \;
+sed -i "s|PROJECT_NAME:=template-golang|PROJECT_NAME:=${project}|g" Makefile
+sed -i "s|BINARY_NAME:=binary|BINARY_NAME:=${binary}|g" Makefile
